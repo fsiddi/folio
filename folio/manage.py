9c0448cf8b64cccbcd732fcf289fb2f135303a7e
Ines Almeida <britalmeida@gmail.com>
2015-01-25 14:05:46 +0000

    alembic

--- folio/manage.py
+++ folio/manage.py
@@ -1,8 +1,13 @@
 import os
 from application import app, db
 from flask.ext.script import Manager
+from flask.ext.migrate import Migrate, MigrateCommand
 
 manager = Manager(app)
+
+# alembic migrations
+migrate = Migrate(app, db)
+manager.add_command('db', MigrateCommand)
 
 @manager.command
 def create_all_tables():
